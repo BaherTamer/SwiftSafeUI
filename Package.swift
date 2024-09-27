04f492a3e98529016ede402c123eebe5fc698ea3
BaherTamer <99125691+BaherTamer@users.noreply.github.com>
2024-09-27 21:48:32 +0300

    chore: update swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,15 +9,14 @@ let package = Package(
         .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftSafeUI",
-            targets: ["SwiftSafeUI"]),
+            targets: ["SwiftSafeUI"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftSafeUI"),
+            name: "SwiftSafeUI"
+        ),
     ]
 )
