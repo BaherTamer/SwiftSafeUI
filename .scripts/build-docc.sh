54885ef726d41952aee6c3c7244d9ab0a8e8b23b
Baher Tamer <baher.appledev@gmail.com>
2024-10-18 22:06:50 +0300

    chore: update docs folder & script (#6)

--- .scripts/build-docc.sh
+++ .scripts/build-docc.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+# Navigate to project root
 cd "$(dirname "$0")/.." || exit
 
+# Clear derived data
+rm -rf "$PWD/.derivedData"
+
+# Build the documentation
 xcrun xcodebuild docbuild \
     -scheme SwiftSafeUI \
     -destination 'generic/platform=iOS Simulator' \
-    -derivedDataPath "$PWD/.derivedData"
+    -derivedDataPath "$PWD/.derivedData" || exit
 
+# Process documentation for static hosting
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/SwiftSafeUI.doccarchive" \
     --output-path ".docs" \
-    --hosting-base-path "SwiftSafeUI"
+    --hosting-base-path "SwiftSafeUI" || exit
 
+# Create redirect index.html
 echo '<script>window.location.href += "/documentation/swiftsafeui"</script>' > .docs/index.html
+
+echo "Documentation successfully updated."
