b3440b798319a1e6e46de65b1bb5f875af817b41
BaherTamer <99125691+BaherTamer@users.noreply.github.com>
2024-10-18 22:27:16 +0300

    Update build-docc.sh

--- .scripts/build-docc.sh
+++ .scripts/build-docc.sh
@@ -2,17 +2,13 @@
 
 cd "$(dirname "$0")/.." || exit
 
-# Clear derived data
-DERIVED_DATA_PATH=$(mktemp -d)
-
-# Build the documentation
 xcrun xcodebuild docbuild \
     -scheme SwiftSafeUI \
     -destination 'generic/platform=iOS Simulator' \
-    -derivedDataPath "$DERIVED_DATA_PATH" || exit
+    -derivedDataPath "$PWD/.derivedData"
 
 xcrun docc process-archive transform-for-static-hosting \
-    "$DERIVED_DATA_PATH/Build/Products/Debug-iphonesimulator/SwiftSafeUI.doccarchive" \
+    "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/SwiftSafeUI.doccarchive" \
     --output-path ".docs" \
     --hosting-base-path "SwiftSafeUI"
 
