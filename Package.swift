7f56d204ceafbf59dbde2480ca94e209e9831e6b
BaherTamer <99125691+BaherTamer@users.noreply.github.com>
2025-02-27 16:35:51 +0200

    chore: update swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,10 +18,10 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "SwiftSafeUI"
+            name: "SwiftSafeUI",
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency=complete")
+            ]
         ),
-    ],
-    swiftLanguageModes: [
-        .v5, .v6
     ]
 )
