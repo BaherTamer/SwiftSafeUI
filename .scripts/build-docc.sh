66cb11a8a8aa152c0873f8744e8fcff1aff4919f
BaherTamer <99125691+BaherTamer@users.noreply.github.com>
2025-03-02 12:02:17 +0200

    Update build-docc.sh

--- .scripts/build-docc.sh
+++ .scripts/build-docc.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-set -e  # Exit on error
-
 cd "$(dirname "$0")/.." || exit
 
-# Store the current branch name
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-# Generate documentation in the current branch
-rm -rf docs
 xcrun xcodebuild docbuild \
     -scheme SwiftSafeUI \
     -destination 'generic/platform=iOS Simulator' \
@@ -16,19 +9,7 @@ xcrun xcodebuild docbuild \
 
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/SwiftSafeUI.doccarchive" \
-    --output-path "docs" \
+    --output-path ".docs" \
     --hosting-base-path "SwiftSafeUI"
 
-echo '<script>window.location.href += "/documentation/swiftsafeui"</script>' > docs/index.html
-
-# Switch to docs branch, keeping changes
-git checkout docs --detach  # Detach HEAD to prevent conflicts
-git checkout docs           # Now switch to docs while keeping changes
-
-# Commit and push only the updated documentation
-git add -f docs  # Force add ignored files if needed
-git commit -m "Update documentation"
-git push origin docs
-
-# Switch back to the original branch
-git checkout "$CURRENT_BRANCH"
+echo '<script>window.location.href += "/documentation/swiftsafeui"</script>' > .docs/index.html
