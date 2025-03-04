8b4f7d58881ab7280bc4743dff1cac637586d8ba
BaherTamer <99125691+BaherTamer@users.noreply.github.com>
2025-03-04 16:52:31 +0200

    doc: update auxiliary modifiers documentation

--- Sources/SwiftSafeUI/Deprecation Handlers/Modifiers/Auxiliary View/SafeNavBarTitle.swift
+++ Sources/SwiftSafeUI/Deprecation Handlers/Modifiers/Auxiliary View/SafeNavBarTitle.swift
@@ -10,77 +10,122 @@
 import SwiftUI
 
 extension View {
+
+    /// Sets the title and display mode in the navigation bar for this view.
     ///
-    /// Sets the navigation bar title for this view, with support for different iOS versions.
-    ///
-    /// - Parameters:
-    ///   - text: The text to be displayed as the navigation bar title.
-    ///   - displayMode: The display mode for the navigation bar title.
-    ///
-    /// - Returns: A view with the specified navigation bar title applied.
-    ///
-    /// This method allows you to set the navigation bar title for a view, adapting to the iOS version:
+    /// This method ensures compatibility across iOS versions:
     /// - On iOS 14 and later, it utilizes the new [`navigationTitle(_:)`](https://developer.apple.com/documentation/swiftui/view/navigationtitle(_:)-5di1u ) method along with [`navigationBarTitleDisplayMode(_:)`](https://developer.apple.com/documentation/swiftui/view/navigationbartitledisplaymode(_:) ).
-    /// - On earlier versions, it falls back to the [`navigationBarTitle(_:displayMode:)`](https://developer.apple.com/documentation/swiftui/view/navigationbartitle(_:displaymode:)-6vw86 ) method.
+    /// - On earlier versions, it falls back to the [`navigationBarTitle(_:displayMode:)`](https://developer.apple.com/documentation/swiftui/view/navigationbartitle(_:displaymode:)-8buvp ) method.
+    ///
+    /// ## Apple Discussion
+    /// Use `safeNavigationBarTitle(_:displayMode:)` to set the title of the navigation bar for this view and specify a display mode for the title from one of the
+    /// [`NavigationBarItem.TitleDisplayMode`](https://developer.apple.com/documentation/swiftui/navigationbaritem/titledisplaymode)
+    /// styles. This modifier only takes effect when this view is inside of and visible within a
+    /// [`NavigationView`](https://developer.apple.com/documentation/swiftui/navigationview).
     ///
     /// ## Example
+    /// In the example below, text for the navigation bar title is provided using a
+    /// [`Text`](https://developer.apple.com/documentation/swiftui/text)
+    /// view. The navigation bar title’s
+    /// [`NavigationBarItem.TitleDisplayMode`](https://developer.apple.com/documentation/swiftui/navigationbaritem/titledisplaymode)
+    /// is set to `.inline` which places the navigation bar title in the bounds of the navigation bar.
+    ///
     /// ```swift
-    /// struct ContentView: View {
-    ///     var body: some View {
-    ///         NavigationView {
-    ///             Text("SwiftSafeUI")
-    ///                 .safeNavBarTitle(
-    ///                     Text("SafeNavBarTitle"),
-    ///                     displayMode: .inline
-    ///                 )
+    /// struct FlavorView: View {
+    ///    let items = ["Chocolate", "Vanilla", "Strawberry", "Mint Chip", "Pistachio"]
+    ///
+    ///    var body: some View {
+    ///         SafeNavigationStack {
+    ///             List(items, id: \.self) {
+    ///                 Text($0)
+    ///             }
+    ///             .safeNavigationBarTitle(
+    ///                 Text("Today's Flavors"),
+    ///                 displayMode: .inline
+    ///             )
     ///         }
     ///     }
     /// }
     /// ```
     ///
-    nonisolated public func safeNavBarTitle(
-        _ text: Text,
+    /// - Parameters:
+    ///   - title: A title for this view to display in the navigation bar.
+    ///   - displayMode: The style to use for displaying the navigation bar title.
+    @ViewBuilder
+    nonisolated public func safeNavigationBarTitle(
+        _ title: Text,
         displayMode: NavigationBarItem.TitleDisplayMode
     ) -> some View {
-        modifier(
-            SafeNavBarTitle(
-                text: text,
+        if #available(iOS 14.0, *) {
+            self
+                .navigationTitle(title)
+                .navigationBarTitleDisplayMode(displayMode)
+        } else {
+            navigationBarTitle(
+                title,
                 displayMode: displayMode
             )
-        )
-    }
-}
-
-private struct SafeNavBarTitle: ViewModifier {
-    // MARK: - Inputs
-    let text: Text
-    let displayMode: NavigationBarItem.TitleDisplayMode
-
-    // MARK: - Body
-    func body(content: Content) -> some View {
-        if #available(iOS 14.0, *) {
-            applyNavigationTitle(content)
-        } else {
-            applyNavigationBarTitle(content)
         }
     }
-}
 
-// MARK: - Private Helpers
-extension SafeNavBarTitle {
-    @available(iOS 14.0, *)
-    private func applyNavigationTitle(_ content: Content) -> some View {
-        content
-            .navigationTitle(text)
-            .navigationBarTitleDisplayMode(displayMode)
-    }
 
-    @available(iOS, introduced: 13.0, deprecated: 14.0)
-    private func applyNavigationBarTitle(_ content: Content) -> some View {
-        content
-            .navigationBarTitle(
-                text,
+    /// Sets the title and display mode in the navigation bar for this view.
+    ///
+    /// This method ensures compatibility across iOS versions:
+    /// - On iOS 14 and later, it utilizes the new [`navigationTitle(_:)`](https://developer.apple.com/documentation/swiftui/view/navigationtitle(_:)-43srq ) method along with [`navigationBarTitleDisplayMode(_:)`](https://developer.apple.com/documentation/swiftui/view/navigationbartitledisplaymode(_:) ).
+    /// - On earlier versions, it falls back to the [`navigationBarTitle(_:displayMode:)`](https://developer.apple.com/documentation/swiftui/view/navigationbartitle(_:displaymode:)-gi7a ) method.
+    ///
+    /// ## Apple Discussion
+    /// Use `safeNavigationBarTitle(_:displayMode:)` to set the title of the navigation bar for this view and specify a display mode for the title from one of the
+    /// [`NavigationBarItem.TitleDisplayMode`](https://developer.apple.com/documentation/swiftui/navigationbaritem/titledisplaymode)
+    /// styles. This modifier only takes effect when this view is inside of and visible within a
+    /// [`NavigationView`](https://developer.apple.com/documentation/swiftui/navigationview).
+    ///
+    /// If the `titleKey` can't be found, the title uses the text of the key name instead.
+    ///
+    /// ## Example
+    /// In the example below, text for the navigation bar title is provided using a
+    /// [`Text`](https://developer.apple.com/documentation/swiftui/text)
+    /// view. The navigation bar title’s
+    /// [`NavigationBarItem.TitleDisplayMode`](https://developer.apple.com/documentation/swiftui/navigationbaritem/titledisplaymode)
+    /// is set to `.inline` which places the navigation bar title in the bounds of the navigation bar.
+    ///
+    /// ```swift
+    /// struct FlavorView: View {
+    ///    let items = ["Chocolate", "Vanilla", "Strawberry", "Mint Chip", "Pistachio"]
+    ///
+    ///    var body: some View {
+    ///         SafeNavigationStack {
+    ///             List(items, id: \.self) {
+    ///                 Text($0)
+    ///             }
+    ///             .safeNavigationBarTitle(
+    ///                 "Today's Flavors",
+    ///                 displayMode: .inline
+    ///             )
+    ///         }
+    ///     }
+    /// }
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - titleKey: A key to a localized description of this view to display in the navigation bar.
+    ///   - displayMode: The style to use for displaying the navigation bar title.
+    @ViewBuilder
+    nonisolated public func safeNavigationBarTitle(
+        _ titleKey: LocalizedStringKey,
+        displayMode: NavigationBarItem.TitleDisplayMode
+    ) -> some View {
+        if #available(iOS 14.0, *) {
+            self
+                .navigationTitle(titleKey)
+                .navigationBarTitleDisplayMode(displayMode)
+        } else {
+            navigationBarTitle(
+                titleKey,
                 displayMode: displayMode
             )
+        }
     }
+
 }
