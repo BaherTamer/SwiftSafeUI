7b09754fc2602ec72cffe1ff19eedc3a2a0a6833
BaherTamer <99125691+BaherTamer@users.noreply.github.com>
2025-03-07 00:28:29 +0200

    fix: package not compatible below swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,14 +14,16 @@ let package = Package(
             targets: [
                 "SwiftSafeUI"
             ]
-        ),
+        )
     ],
     targets: [
         .target(
-            name: "SwiftSafeUI"
-        ),
-    ],
-    swiftLanguageModes: [
-        .v5, .v6
+            name: "SwiftSafeUI",
+            swiftSettings: [
+                .enableExperimentalFeature(
+                    "StrictConcurrency=complete"
+                )
+            ]
+        )
     ]
 )
